803afa1406a7fe6dc811ab669c368ef3620b013a
Marta Sleboda <marta.sleboda@ttpsc.pl>
2023-05-04 22:30:18 +0200

    modify backend settings

--- version.tf
+++ version.tf
@@ -11,10 +11,10 @@ terraform {
   }
 
   backend "azurerm" {
+    resource_group_name  = "rg-martha-main-001"
     storage_account_name = "stmarthamain001"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    use_azuread_auth     = true
     tenant_id            = "075ddcce-4e47-4d33-843f-c6b7fbc0ba52"
     subscription_id      = "ac10c819-2836-4539-a08d-cf5c42a71d7a" # sub-edu-001
   }
